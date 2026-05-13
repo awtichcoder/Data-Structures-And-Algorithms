4c0b715dedaa15f276496acb1c530b83ca39d34b
kiet <mkla1252006@gmail.com>
2026-05-13 13:51:29 +0700

    done List

---
+++ 4-List/4-List/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 khoi tao List
+            List<int> ds1 = new List<int>();
+            List<float> ds2 = new List<float>();
+            // 2 gan phan tu
+            List<int> ds3 = new List<int>() { 1,2,31,32,232,453,2,3,4};
+            // 3 in ra phan tu
+            foreach( int i in ds3)
+            {
+                Console.Write($"{i}  ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------");
+            // Add
+            List<int> ds4 = new List<int>();
+            ds4.Add(100);
+            ds4.Add(0);
+            ds4.Add(102);
+            ds4.ForEach(i => Console.Write(i+" "));
+            
+            // Remove xoa phan tu dau tieen xuat hien trong mang
+            Console.WriteLine();
+            ds4.Remove(0);
+            ds4.ForEach(i => {
+                // cac yeu cau khac
+                Console.Write(i + " ");
+            });
+            Console.WriteLine();
+            //Count
+            Console.WriteLine(" so phan tu trong mang ds4 la: " +ds4.Count);
+            //Clear
+            
+            ds4.Clear();
+            Console.WriteLine(" ds4 da bi clear()");
+            ds4.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+            //AddRange()
+            Console.WriteLine(" ds5 sao khi them cao ds6");
+            List<int> ds5 = new List<int>() {1,42,23 };
+            List<int> ds6 = new List<int>() { 43,232,3};
+            ds5.AddRange(ds6);
+            ds5.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+            // Contains : gia tri co ton tai hay khom
+            bool kq = ds5.Contains(3);
+            Console.WriteLine(kq);
+            // Insert(index, value)
+            List<int> ds7 = new List<int>() { 1, 42, 23 };
+            ds7.Insert(1, 34);
+            ds7.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+
+            //RemoveAt( int index) xoa phan tu vi tri index chi dinh
+            ds7.RemoveAt(1);
+            ds7.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+            //Reverse()
+            ds7.Reverse();
+            ds7.ForEach(i => Console.Write(i + " "));
+            Console.WriteLine();
+            // BinarySearch()
+            Console.WriteLine();
+            List<int> ds8 = new List<int>() { 1, 42, 23 };
+            int kq2 = ds8.BinarySearch(4234);
+            Console.WriteLine("ket qua + "+kq2);
+
+
+
+
+
+
+        }
+    }
+}
---
+++ 4-List/4-List/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("4-List")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4-List")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b784dfd1-48fa-4632-9325-167e4bed5e8d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
